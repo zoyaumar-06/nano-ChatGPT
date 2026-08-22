c1c91ae4c474e58d2161a5096c0e84ca0666da39
Zoya Umar <zoyaumar2006@gmail.com>
2026-08-22 15:10:26 +0500

    Add files via upload

---
+++ configurator.py
@@ -0,0 +1,31 @@
+import sys
+from ast import literal_eval
+
+for arg in sys.argv[1:]:
+    if '=' not in arg:
+        
+        assert not arg.startswith('--')
+        config_file = arg
+        print(f"Overriding config with {config_file}:")
+        with open(config_file) as f:
+            print(f.read())
+        exec(open(config_file).read())
+    else:
+        
+        assert arg.startswith('--')
+        key, val = arg.split('=')
+        key = key[2:]
+        if key in globals():
+            try:
+                
+                attempt = literal_eval(val)
+            except (SyntaxError, ValueError):
+                
+                attempt = val
+            
+            assert type(attempt) == type(globals()[key])
+            
+            print(f"Overriding: {key} = {attempt}")
+            globals()[key] = attempt
+        else:
+            raise ValueError(f"Unknown config key: {key}")
